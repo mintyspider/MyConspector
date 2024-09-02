9822f8a639ebce9511c984d7c3d99967aec6870c
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-02 22:08:42 +0300

    ui finished

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../imgs/full-logo.png'
 import InputBox from '../components/input.component'
+import google from '../imgs/google.png'
+import { Link } from 'react-router-dom'
 
 const AuthForm = ({type}) => {
   return (
@@ -36,6 +38,32 @@ const AuthForm = ({type}) => {
                 placeholder="Пароль"
                 required
             />
+
+            <button className='btn-dark center mt-14' type="submit">
+                {type == 'sign-in'? 'Войти' : 'Зарегистрироваться'}
+            </button>
+
+            <div className='relative w-full flex items-center gap-2 my-10 opacity-10 text-black font-bold'>
+                <hr className='w-1/2 border-black' />
+                <p>или</p>
+                <hr className='w-1/2 border-black' />
+            </div>
+
+            <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
+                <img src={google} alt="" className='w-5'/>
+                Открыть Google
+            </button>
+
+            {
+                type == 'sign-in'?
+                <p className='text-center mt-6 text-dark-grey text-xl'>
+                    Нет аккаунта? <Link to="/sign-up" className='hover:underline text-xl text-dark-grey ml-1'>Зарегистрироваться</Link>
+                </p>
+                :
+                <p className='text-center mt-6 text-dark-grey text-xl'>
+                    Уже есть аккаунт? <Link to="/sign-in" className='hover:underline text-xl text-dark-grey ml-1'>Войти</Link>
+                </p>
+            }
 
         </form>
     </section>
