e2ab8c7f7bd11be31f80608e4ba5b384102fab70
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-03 21:07:33 +0300

    validating ui input

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from '../imgs/full-logo.png'
 import InputBox from '../components/input.component'
 import google from '../imgs/google.png'
@@ -6,11 +6,52 @@ import { Link } from 'react-router-dom'
 import AnimationWrapper from '../common/page-animation'
 
 const AuthForm = ({ type }) => {
+
+    const auth = useRef();
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        //formData
+        let form = new FormData(auth.current);
+        let formData = {};
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+        for(let [key, value] of form.entries()){
+            formData[key] = value;
+        }
+
+        let { fullname, email, password } = formData;
+        // validation
+        if (fullname){
+            if (fullname.length < 3) {
+                return console.log({ 'error': 'Имя должно содержать хотя бы 3 символа' });
+            }
+        }
+        if (!email.length) {
+            return console.log({ 'error': 'Введите электронную почту' });
+        }
+
+        if (!emailRegex.test(email)) {
+            return console.log({ 'error': 'Неверный формат электронной почты' });
+        }
+
+        if (!password.length) {
+            return console.log({ 'error': 'Введите пароль' });
+        }
+
+        if (!passwordRegex.test(password)) {
+            return console.log({
+                'error': 'Пароль должен состоять не менее чем из 6 символов и содержать как минимум одну заглавную букву, одну строчную букву и одну цифру'
+            });
+        }
+    }
   return (
     <AnimationWrapper key={type}>
         <section className={'h-cover flex items-center justify-center md:justify-around flex-col ' + ( type=='sign-in' ? 'md:flex-row' : 'md:flex-row-reverse')}>
             <img src={logo} alt="" className='w-[20%] mb-3 md:w-[49%] md:mb-0'/>
-            <form className='w-[80%] max-w-[400px] md:w-[49%]'>
+            <form ref={auth} className='w-[80%] max-w-[400px] md:w-[49%]'>
                 <h1 className='text-4xl font-gelasio text-center mb-24'>
                     {type == 'sign-in' ? 'Входи и действуй!' : 'Здесь все начинается'}
                 </h1>
@@ -41,7 +82,10 @@ const AuthForm = ({ type }) => {
                     required
                 />
 
-                <button className='btn-dark center mt-14' type="submit">
+                <button className='btn-dark center mt-14' 
+                type="submit"
+                onClick={handleSubmit}
+                >
                     {type == 'sign-in'? 'Войти' : 'Зарегистрироваться'}
                 </button>
 
