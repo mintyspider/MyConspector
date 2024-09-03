1fd3cecf1010da7a3bb3cc77924569516d3aa26d
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-03 23:41:41 +0300

    added firebase

---
+++ frontend/src/common/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3VzQnOqLwFVkb4zDQysHnhwL7b5O2bTQ",
+  authDomain: "myconspector.firebaseapp.com",
+  projectId: "myconspector",
+  storageBucket: "myconspector.appspot.com",
+  messagingSenderId: "235037755331",
+  appId: "1:235037755331:web:f97c1443ae3613cf82df76"
+};
+
+const app = initializeApp(firebaseConfig);
