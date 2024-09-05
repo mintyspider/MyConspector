35c442006c648fd67838595c5ba4299ac5f87954
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-05 12:58:32 +0300

    editor navbar

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../imgs/logo.png"
 
 const BlogEditor = () => {
   return (
-    <div>
-      this is editor page
-    </div>
+    <>
+    <nav className='navbar'>
+            <Link to="/" className='flex-none w-12'>
+                <img src={logo} />
+            </Link>
+            <p className='max-md:hidden text-black line-clamp-1 w-full text-bold text-2xl'>Новый конспект</p>
+
+            <div className='flex gap-4 ml-auto'>
+                <button className='btn-dark py-2 flex gap-2'>
+                <i className="fi fi-rr-file-upload mt-1"></i>
+                    Опубликовать
+                </button>
+                <button className='btn-light py-2 flex gap-2'>
+                <i className="fi  fi-rr-disk text-dark-grey mt-1"></i>
+                    Сохранить черновик
+                </button>
+            </div>
+    </nav>
+    </>
   )
 }
 
