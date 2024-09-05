f1abb73113c182c7f83ff112b9fd0c2ac889f318
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-05 13:05:29 +0300

    editor page

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../imgs/logo.png"
+import AnimationWrapper from '../common/page-animation'
 
 const BlogEditor = () => {
   return (
     <>
-    <nav className='navbar'>
-            <Link to="/" className='flex-none w-12'>
-                <img src={logo} />
-            </Link>
-            <p className='max-md:hidden text-black line-clamp-1 w-full text-bold text-2xl'>Новый конспект</p>
+        <nav className='navbar'>
+                <Link to="/" className='flex-none w-12'>
+                    <img src={logo} />
+                </Link>
+                <p className='max-md:hidden text-black line-clamp-1 w-full text-bold text-2xl'>Новый конспект</p>
 
-            <div className='flex gap-4 ml-auto'>
-                <button className='btn-dark py-2 flex gap-2'>
-                <i className="fi fi-rr-file-upload mt-1"></i>
-                    Опубликовать
-                </button>
-                <button className='btn-light py-2 flex gap-2'>
-                <i className="fi  fi-rr-disk text-dark-grey mt-1"></i>
-                    Сохранить черновик
-                </button>
-            </div>
-    </nav>
+                <div className='flex gap-4 ml-auto'>
+                    <button className='btn-dark py-2 flex gap-2'>
+                    <i className="fi fi-rr-file-upload mt-1"></i>
+                        Опубликовать
+                    </button>
+                    <button className='btn-light py-2 flex gap-2'>
+                    <i className="fi  fi-rr-disk text-dark-grey mt-1"></i>
+                        Сохранить черновик
+                    </button>
+                </div>
+        </nav>
+
+        <AnimationWrapper>
+            <section>
+                <div className='mx-auto max-w-[900px] w-full'>
+                    <div className='relative aspect-video bg-white border-4 border-grey hover:opacity-80'>
+                        <label>
+                            <input id="uploadBanner"/>
+                        </label>
+                    </div>
+                </div>
+            </section>
+        </AnimationWrapper>
     </>
   )
 }
