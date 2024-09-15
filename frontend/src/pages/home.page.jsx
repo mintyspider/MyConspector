aaab77e8e3eeb10c77b1cb73ce34fe87db78d709
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-15 20:37:12 +0300

    HOME PAGE

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -8,7 +8,10 @@ const HomePage = () => {
         <section className='h-cover flex justify-center gap-10'>
             {/* Последние записи */}
             <div className='w-full'>
-                <InPageNavigation routes={["Новое", "Популярное"]}></InPageNavigation>
+                <InPageNavigation routes={["Новое", "Популярное"]} defaultHidden={["Популярное"]}>
+                  <h1>Homepage</h1>
+                  <h1>Trendpage</h1>
+                </InPageNavigation>
             </div>
             {/* Фильтры и самые популярные конспекты */}
             <div></div>
