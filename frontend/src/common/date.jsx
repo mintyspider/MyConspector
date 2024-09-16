211ff3aa3203d0c942fc94eda94e5f2fb4682761
Asya Larionova <m1ntyspider@yandex.ru>
2024-09-16 09:10:47 +0300

    HOME PAGE

---
+++ frontend/src/common/date.jsx
@@ -0,0 +1,9 @@
+let months = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];
+
+let days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+
+export const getDay = (timestamp) => {
+    let date = new Date(timestamp);
+
+    return `${date.getDate()} ${months[date.getMonth()]}`
+}
--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import AnimationWrapper from '../common/page-animation'
 import InPageNavigation from '../components/inpage-navigation.component'
+import Loader from '../components/loader.component'
+import BlogPostCard from '../components/blog-post.component'
 
 const HomePage = () => {
+
+  let [blogs, setBlogs] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latestblogs")
+    .then(({ data }) => {
+      setBlogs(data.blogs)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, [])
+
   return (
     <AnimationWrapper>
         <section className='h-cover flex justify-center gap-10'>
             {/* Последние записи */}
             <div className='w-full'>
                 <InPageNavigation routes={["Новое", "Популярное"]} defaultHidden={["Популярное"]}>
-                  <h1>Homepage</h1>
+                  <>
+                    {
+                      blogs == null 
+                      ? <Loader />
+                      : blogs.map((blog, i) => {
+                        return <AnimationWrapper transition={{duration: 1, delay: i*.1 }} key={i}>
+                          <BlogPostCard content={blog} author={blog.author.personal_info}/>
+                        </AnimationWrapper>
+                      })
+                    }
+                  </>
                   <h1>Trendpage</h1>
                 </InPageNavigation>
             </div>
